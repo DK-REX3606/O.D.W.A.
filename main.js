66bd83fe8aed3b19b8def1e98caa1f09e178ba08
DK-REX3606 <71270037+DK-REX3606@users.noreply.github.com>
2020-10-01 18:37:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+img="";
+status="";
+objects=[];
+function preload(){
+    img=loadImage('dog_cat.jpg');
+}
+function setup(){
+    canvas=createCanvas(500,500);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(380,380);
+    video.hide();
+    objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="Status: Detecting Objects";
+}
+function draw(){
+    image(video,0,0,500,500);
+    if (status!="") {
+        r=random(255);
+        g=random(255);
+        b=random(255);
+        objectDetector.detect(video,gotResult);
+        for (i=0;i<objects.length;i++) {
+            document.getElementById("status").innerHTML="Status: Object Detected";
+            document.getElementById("number_of_objects").innerHTML="No. of objects are "+objects.length;
+            fill(r,g,b);
+            percent=floor(objects[i].confidence*100);
+            text(objects[i].label+" "+percent+"%",objects[i].x+15,objects[i].y+15);
+            noFill();
+            stroke("green");
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+        }
+    }
+}
+function modelLoaded(){
+    console.log("object detector is intialized");
+    status=true;
+}
+function gotResult(error,results){
+    if (error) {
+        console.error(error);
+    }
+    console.log(results);
+    objects=results;
+}
