ba277e69fd6c7d91fcb42ac4e4b514067dd799ea
DK-REX3606 <71270037+DK-REX3606@users.noreply.github.com>
2020-10-22 08:08:27 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,44 +1,45 @@
-objects = [];
-status = "";
-function preload() {
-    video = createVideo('video.mp4');
+img="";
+status="";
+objects=[];
+function preload(){
+    img=loadImage('dog_cat.jpg');
 }
-function setup() {
-    canvas = createCanvas(480, 380);
+function setup(){
+    canvas=createCanvas(500,500);
     canvas.center();
+    video=createCapture(VIDEO);
+    video.size(380,380);
     video.hide();
+    objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="Status: Detecting Objects";
 }
-function start() {
-    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
-    document.getElementById("status").innerHTML = "Status: Detecting Objects";
-}
-function modelLoaded() {
-    console.log("model loaded");
-    status = true;
-    video.loop();
-    video.speed(1);
-    video.volume(0);
-}
-function draw() {
-    image(video, 0, 0, 480, 380);
-    if (status != "") {
-        objectDetector.detect(video, gotResult);
-        for (i = 0; i < objects.length; i++) {
-            document.getElementById("status").innerHTML = "Status: Objects Detected";
-            document.getElementById("number_of_objects").innerHTML = "Number of objects detected are" + objects.length;
-            fill("purple");
-            percent = floor(objects[i].confidence * 100);
-            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+function draw(){
+    image(video,0,0,500,500);
+    if (status!="") {
+        r=random(255);
+        g=random(255);
+        b=random(255);
+        objectDetector.detect(video,gotResult);
+        for (i=0;i<objects.length;i++) {
+            document.getElementById("status").innerHTML="Status: Object Detected";
+            document.getElementById("number_of_objects").innerHTML="No. of objects are "+objects.length;
+            fill(r,g,b);
+            percent=floor(objects[i].confidence*100);
+            text(objects[i].label+" "+percent+"%",objects[i].x+15,objects[i].y+15);
             noFill();
-            stroke("yellow");
-            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+            stroke("green");
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
         }
     }
 }
-function gotResult(results, error) {
+function modelLoaded(){
+    console.log("object detector is intialized");
+    status=true;
+}
+function gotResult(error,results){
     if (error) {
         console.error(error);
     }
     console.log(results);
-    objects = results;
+    objects=results;
 }
